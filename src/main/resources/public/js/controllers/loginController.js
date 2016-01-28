21dbca9e8e921e1ff73f5034c2040f30b0ed3578
Rick Hanberry <rhanberry@catalystitservices.com>
2016-01-28 14:02:13 -0800

    added new login page and controller, need to update routes.js

--- src/main/resources/public/js/controllers/loginController.js
+++ src/main/resources/public/js/controllers/loginController.js
@@ -1,3 +1,27 @@
-angular.module('expenseApp').controller('loginController', ['$scope', '$state', '$http', function($scope, $state, $http){
-	
-}]);
+'use strict';
+
+angular.module('expenseApp')
+    .controller('LoginController', function ($rootScope, $scope, $state, $timeout, Auth) {
+        $scope.user = {};
+        $scope.errors = {};
+
+        //$scope.rememberMe = true;
+        $timeout(function (){angular.element('[ng-model="username"]').focus();});
+        $scope.login = function (event) {
+            event.preventDefault();
+            Auth.login({
+                username: $scope.username,
+                password: $scope.password,
+
+            }).then(function () {
+                $scope.authenticationError = false;
+                if ($rootScope.previousStateName === 'register') {
+                    $state.go('home');
+                } else {
+                    $rootScope.back();
+                }
+            }).catch(function () {
+                $scope.authenticationError = true;
+            });
+        };
+    });
