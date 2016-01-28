243882f4733b4c3ac151ba92bd268c224137d340
Billy Horn <bHorn@catalystitservices.com>
2016-01-28 14:03:22 -0800

    Register working

--- src/main/java/com/catalyst/springboot/controllers/HomeController.java
+++ src/main/java/com/catalyst/springboot/controllers/HomeController.java
@@ -9,35 +9,6 @@ public class HomeController {
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home() {
-		return employeeIndex();
-	}
-
-	@RequestMapping(value = "/about-us", method = RequestMethod.GET)
-	public String about() {
-
-		return "/about.html";
-	}
-
-	@RequestMapping(value = "/employee/index", method = RequestMethod.GET)
-	public String employeeIndex() {
-		return "/employee-index.html";
-	}
-
-	@RequestMapping(value = "/employee/create", method = RequestMethod.GET)
-	public String employeeCreate() {
-		return "/employee-create.html";
-	}
-	@RequestMapping(value = "/employee/update", method = RequestMethod.GET)
-	public String employeeUpdate() {
-		return "/employee-update.html";
-	}
-	@RequestMapping(value = "/loginpage", method = RequestMethod.GET)
-	public String login() {
-		return "/login.html";
-	}
-
-	@RequestMapping(value = "/employee/delete", method = RequestMethod.GET)
-	public String employeeDelete() {
-		return "/employee-delete.html";
+		return "/index.html";
 	}
 }
