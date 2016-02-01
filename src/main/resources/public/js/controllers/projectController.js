8e6699305ce93131c5b11090a5fc4189609f8fdf
Billy Horn <bHorn@catalystitservices.com>
2016-02-01 09:07:52 -0800

    register working bit of work on creating project

--- src/main/resources/public/js/controllers/projectController.js
+++ src/main/resources/public/js/controllers/projectController.js
@@ -1,3 +1,9 @@
 angular.module('expenseApp').controller('projectController', ['$scope', '$state', '$http', function($scope, $state, $http){
-	
+	$scope.test = function(){
+		var obj = {name: 'misko', gender: 'male'};
+		var log = [];
+		angular.forEach(obj, function(value, key) {
+		  console.log(key + ': ' + value);
+		});
+	}	
 }]);
