3cd2bdedddda3761cd06c3f2ac026d15454e9e9f
Gregory Walpole <gWalpole@catalystitservices.com>
2016-02-02 14:29:05 -0800

    EndUser javaDoc comments to satiate the Qube

--- src/main/java/com/catalyst/springboot/entities/EndUser.java
+++ src/main/java/com/catalyst/springboot/entities/EndUser.java
@@ -19,8 +19,12 @@ import javax.validation.constraints.NotNull;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 
+/**
+ * An object representing individual users of the application.
+ * @author ldahlberg
+ * @author gwalpole
+ */
 @Entity
-
 public class EndUser {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -48,38 +52,94 @@ public class EndUser {
 	@JoinColumn(name="springroleid")
 	private SpringRole springrole;
 	
-	
+	/**
+	 * Generic setter for userId
+	 * @param userId
+	 */
 	public void setUserId(Integer userId) {
 		this.userId = userId;
 	}
-	
+	/**
+	 * Generic getter for userId
+	 * @return
+	 */
 	public Integer getUserId() {
 		return userId;
 	}
-
+	/**
+	 * Generic getter for firstname
+	 * @return
+	 */
 	public String getFirstname() {
 		return firstname;
 	}
+	/**
+	 * Generic setter for firstname
+	 * @param firstname
+	 */
 	public void setFirstname(String firstname) {
 		this.firstname = firstname;
 	}
+	/**
+	 * Generic getter for lastname
+	 * @return
+	 */
 	public String getLastname() {
 		return lastname;
 	}
+	/**
+	 * Generic setter for lastname
+	 * @param lastname
+	 */
 	public void setLastname(String lastname) {
 		this.lastname = lastname;
 	}
-		
+	/**
+	 * Generic setter for password
+	 * @param password
+	 */
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
-	
+	/**
+	 * Generic getter for password
+	 * @return
+	 */
 	public String getPassword() {
 		return password;
 	}
+	/**
+	 * Generic getter for email
+	 * @return
+	 */
+	public String getEmail() {
+		return email;
+	}
+	/**
+	 * Generic setter for email
+	 * @param email
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	/**
+	 * Generic getter for isActive
+	 * @return
+	 */
+	public Boolean getIsActive() {
+		return isActive;
+	}
+	/**
+	 * Generic setter for isActive
+	 * @param isActive
+	 */
+	public void setIsActive(Boolean isActive) {
+		this.isActive = isActive;
+	}
 	
-	
+	/**
+	 * Auto-generated hashCode function 
+	 */
 	@Override
 	public int hashCode() {
 		HashCodeBuilder builder = new HashCodeBuilder(31, 17);
@@ -87,6 +147,9 @@ public class EndUser {
 		return builder.toHashCode();
 	}
 
+	/**
+	 * auto-generated equals function
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if(!(obj instanceof EndUser)){
@@ -98,22 +161,6 @@ public class EndUser {
 		builder.append(this.userId, user.userId);
 		return builder.isEquals();
 		
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public Boolean getIsActive() {
-		return isActive;
-	}
-
-	public void setIsActive(Boolean isActive) {
-		this.isActive = isActive;
 	}
 
 }
