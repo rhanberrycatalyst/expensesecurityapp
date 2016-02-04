2b633ef49d04615f469ae1f360ac5a7113ce8775
Gregory Walpole <gWalpole@catalystitservices.com>
2016-02-04 11:30:13 -0800

    Added very basic report list functionality to home page

--- src/main/resources/public/js/controllers/homeController.js
+++ src/main/resources/public/js/controllers/homeController.js
@@ -1,3 +1,14 @@
-angular.module('expenseApp').controller('homeController', ['$scope', '$state', '$http', function($scope, $state, $http){
+angular.module('expenseApp').controller('homeController', ['$scope', '$state', '$http', 'getReportService', function($scope, $state, $http, getReportService){
+	
+	$scope.reportList = {};
+	getReportService.dbGetAll(1).then(
+			  function(success){
+				  $scope.reportList = success.data;
+				  console.log(success.data);
+				  return success.data;
+			  },function(error){
+				  console.log("Bad");  
+			  }
+			  );
 	
 }]);
--- src/main/resources/public/js/services/getReportService.js
+++ src/main/resources/public/js/services/getReportService.js
@@ -1,8 +1,9 @@
 angular.module("expenseApp").service("getReportService", ["$http", function($http){
 	
-	var dbCall = function(id){ return $http.get('/reports/'+id)};
+	var dbCall = function(id){ return $http.get('/reportlist/'+id)};
 //	angular.forEach(reportObj, function(value, key){
 //	});
+	var dbGetAll = function(userid){ return $http.get('/reports/'+userid)};
 	
-	return {dbCall: dbCall};
+	return {dbCall: dbCall, dbGetAll: dbGetAll};
 }]);
