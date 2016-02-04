dc2425cb2f8d6c90a51a13ad4c5a3ce546d99412
Rick Hanberry <rhanberry@catalystitservices.com>
2016-02-04 13:10:46 -0800

    Totally changed login frontend and angular project structure.  routing, etc works, but login is not authenticating

---
+++ src/main/java/com/catalyst/springboot/security/Http401UnauthorizedEntryPoint.java
@@ -0,0 +1,31 @@
+package com.catalyst.springboot.security;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+/**
+ * Returns a 401 error code (Unauthorized) to the client.
+ */
+@Component
+public class Http401UnauthorizedEntryPoint implements AuthenticationEntryPoint {
+
+   
+
+    /**
+     * Always returns a 401 error code to the client.
+     */
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException arg2)
+        throws IOException,
+        ServletException {
+
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Access Denied");
+    }
+}
--- src/main/java/com/catalyst/springboot/security/SpringSecurityConfigurer.java
+++ src/main/java/com/catalyst/springboot/security/SpringSecurityConfigurer.java
@@ -3,55 +3,158 @@ package com.catalyst.springboot.security;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CsrfFilter;
 
-@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
-@EnableWebMvcSecurity
+import com.catalyst.springboot.web.filter.CsrfCookieGeneratorFilter;
+
+//@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+//@EnableWebMvcSecurity
+//@Configuration
+//public class SpringSecurityConfigurer extends WebSecurityConfigurerAdapter {
+//
+//	@Autowired
+//	private CustomAuthenticationFailureHandler authenticationFailureHandler;
+//
+//	@Bean
+//	CustomAuthenticationFailureHandler authenticationHandler() {
+//		return new CustomAuthenticationFailureHandler();
+//	}
+//
+//	/**
+//	 * @param authFailure
+//	 *            the authFailure to set
+//	 */
+//	public void setAuthFailure(CustomAuthenticationFailureHandler authFailure) {
+//		this.authenticationFailureHandler = authFailure;
+//	}
+//
+//	@Autowired
+//	private CustomLogOutSuccessHandler logoutSuccessHandler;
+//
+//	@Bean
+//	CustomLogOutSuccessHandler logoutSuccessHandler() {
+//		return new CustomLogOutSuccessHandler();
+//	}
+//
+//	/**
+//	 * @param logoutSuccess
+//	 *            the logoutSuccess to set
+//	 */
+//	public void setLogOutSuccess(CustomLogOutSuccessHandler logoutSuccess) {
+//		this.logoutSuccessHandler = logoutSuccess;
+//	}
+//
+//	@Override
+//	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+//
+//		auth.inMemoryAuthentication().withUser("user").password("root").authorities("user");
+//
+//		auth.jdbcAuthentication().dataSource(datasource).passwordEncoder(encoder())
+//				.usersByUsernameQuery("SELECT email,password,isactive FROM enduser WHERE email=?")
+//				.authoritiesByUsernameQuery(
+//						"SELECT enduser.email,springrole.role FROM enduser JOIN springrole ON enduser.springroleid =springrole.roleid WHERE enduser.email=?");// this
+//																																								// fakes
+//																																								// a
+//																																								// user
+//																																								// role
+//
+//	}
+//
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		http
+//			.authorizeRequests()
+//				.antMatchers("/resources/**").permitAll()
+//				.anyRequest().authenticated()
+//				.and()
+//			.formLogin()
+//				.loginPage("/")
+//				.permitAll()
+//				.defaultSuccessUrl("/index.html")
+//				.usernameParameter("username")
+//				.passwordParameter("password")
+//				.failureHandler(authenticationFailureHandler)
+//				.and()
+//			.headers()
+//				.cacheControl()
+//				.and()
+//			.logout()
+//				.logoutSuccessHandler(logoutSuccessHandler)
+//				.logoutSuccessUrl("/")
+//				.deleteCookies("JSESSIONID", "CSRF-TOKEN")
+//				.permitAll()
+//				.and()
+//			.csrf().disable();
+//
+//	}
+//
+//	@Bean
+//	public BCryptPasswordEncoder encoder() {
+//		return new BCryptPasswordEncoder();
+//	}
+//
+//	/**
+//	 * Tells the Websecurity to ignore the css, js, and pics folders.
+//	 */
+//	 @Override
+//	 public void configure(WebSecurity web) throws Exception {
+//	 web.ignoring().antMatchers("resources/css/**", "resources/js/**", "resources/pics/**");
+//	
+//	 }
+//
+//	@Autowired
+//	private DataSource datasource;
+//
+//	public void setDatasource(DataSource datasource) {
+//		this.datasource = datasource;
+//	}
+//
+//}
 @Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class SpringSecurityConfigurer extends WebSecurityConfigurerAdapter {
 
-	@Autowired
-	private CustomAuthenticationFailureHandler authenticationFailureHandler;
+    
 
-	@Bean
-	CustomAuthenticationFailureHandler authenticationHandler() {
-		return new CustomAuthenticationFailureHandler();
-	}
+    @Autowired
+    private AjaxAuthenticationSuccessHandler ajaxAuthenticationSuccessHandler;
 
-	/**
-	 * @param authFailure
-	 *            the authFailure to set
+    @Autowired
+    private AjaxAuthenticationFailureHandler ajaxAuthenticationFailureHandler;
+
+    @Autowired
+    private AjaxLogoutSuccessHandler ajaxLogoutSuccessHandler;
+
+    @Autowired
+    private Http401UnauthorizedEntryPoint authenticationEntryPoint;
+
+    
+    /**
+	 * Tells the Websecurity to ignore the css, js, and pics folders.
 	 */
-	public void setAuthFailure(CustomAuthenticationFailureHandler authFailure) {
-		this.authenticationFailureHandler = authFailure;
-	}
+	 @Override
+	 public void configure(WebSecurity web) throws Exception {
+	 web.ignoring().antMatchers("resources/css/**", "resources/js/**", "resources/pics/**").antMatchers("/scripts/**/*.{js,html}")
+     .antMatchers("/bower_components/**")
+     .antMatchers("/test/**");
+	
+	 }
+    
 
-	@Autowired
-	private CustomLogOutSuccessHandler logoutSuccessHandler;
+   
 
-	@Bean
-	CustomLogOutSuccessHandler logoutSuccessHandler() {
-		return new CustomLogOutSuccessHandler();
-	}
-
-	/**
-	 * @param logoutSuccess
-	 *            the logoutSuccess to set
-	 */
-	public void setLogOutSuccess(CustomLogOutSuccessHandler logoutSuccess) {
-		this.logoutSuccessHandler = logoutSuccess;
-	}
-
-	@Override
+    @Override
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 
 		auth.inMemoryAuthentication().withUser("user").password("root").authorities("user");
@@ -66,48 +169,49 @@ public class SpringSecurityConfigurer extends WebSecurityConfigurerAdapter {
 																																								// role
 
 	}
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .csrf()
+        .and()
+            .addFilterAfter(new CsrfCookieGeneratorFilter(), CsrfFilter.class)
+            .exceptionHandling()
+            .accessDeniedHandler(new CustomAccessDeniedHandler())
+            .authenticationEntryPoint(authenticationEntryPoint)
+            .and()
+            .authorizeRequests()
+            .antMatchers("/login").permitAll()
+            .antMatchers("/register").permitAll()
+            .antMatchers("/authenticate").permitAll()
+        .and()
+            .formLogin()
+            .loginProcessingUrl("/login")
+            .successHandler(ajaxAuthenticationSuccessHandler)
+            .failureHandler(ajaxAuthenticationFailureHandler)
+            .usernameParameter("username")
+            .passwordParameter("password")
+            .permitAll()
+        .and()
+            .logout()
+            .logoutUrl("/logout")
+            .logoutSuccessHandler(ajaxLogoutSuccessHandler)
+            .deleteCookies("JSESSIONID", "CSRF-TOKEN")
+            .permitAll()
+        .and()
+            .headers()
+            .frameOptions()
+            .disable();
+            
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http
-			.authorizeRequests()
-				.antMatchers("/resources/**").permitAll()
-				.anyRequest().authenticated()
-				.and()
-			.formLogin()
-				.loginPage("/")
-				.permitAll()
-				.defaultSuccessUrl("/index.html")
-				.usernameParameter("username")
-				.passwordParameter("password")
-				.failureHandler(authenticationFailureHandler)
-				.and()
-			.headers()
-				.cacheControl()
-				.and()
-			.logout()
-				.logoutSuccessHandler(logoutSuccessHandler)
-				.logoutSuccessUrl("/")
-				.deleteCookies("JSESSIONID", "CSRF-TOKEN")
-				.permitAll()
-				.and()
-			.csrf().disable();
+    }
 
-	}
-
-	@Bean
-	public BCryptPasswordEncoder encoder() {
-		return new BCryptPasswordEncoder();
-	}
-
-	/**
-	 * Tells the Websecurity to ignore the css, js, and pics folders.
-	 */
-	 @Override
-	 public void configure(WebSecurity web) throws Exception {
-	 web.ignoring().antMatchers("resources/css/**", "resources/js/**", "resources/pics/**");
-	
-	 }
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+    
+    
 
 	@Autowired
 	private DataSource datasource;
@@ -115,5 +219,5 @@ public class SpringSecurityConfigurer extends WebSecurityConfigurerAdapter {
 	public void setDatasource(DataSource datasource) {
 		this.datasource = datasource;
 	}
-
+    
 }
