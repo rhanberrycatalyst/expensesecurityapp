f777c201134f775256ad0945b9c38024fa787aa7
Anu Paulose <APaulose@catalystitservices.com>
2016-02-04 16:37:50 -0800

    changes added

--- src/main/resources/public/scripts/app/viewreport/getReportService.js
+++
@@ -1,9 +0,0 @@
-angular.module("expensereportApp").service("getReportService", ["$http", function($http){
-	
-	return {
-	  dbCall: function(id){ return $http.get('/reportlist/'+id)},
-	  dbGetAll: function(userid){ return $http.get('/reports/'+userid)},
-	  setReport: function(id) {this.curReport = id;},
-	  curReport: 0
-	  };
-}]);
