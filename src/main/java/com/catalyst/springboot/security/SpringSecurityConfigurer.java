2f9864caa5a70fb6b244906e995680bc51adaefe
Luke <lDahlberg@catalystitservices.com>
2016-02-05 08:48:40 -0800

    Minor modifications

--- src/main/java/com/catalyst/springboot/security/SpringSecurityConfigurer.java
+++ src/main/java/com/catalyst/springboot/security/SpringSecurityConfigurer.java
@@ -76,8 +76,6 @@ public class SpringSecurityConfigurer extends WebSecurityConfigurerAdapter {
 
 		http
 		.authorizeRequests()
-		.antMatchers("/resources/**").permitAll()
-		.anyRequest().authenticated()
 		.and()
 	.formLogin()
 		.loginPage("/login")
