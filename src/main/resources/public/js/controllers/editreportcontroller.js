88efc11d422b53ef400d87ebfd31189934bff0bc
Rick Hanberry <rhanberry@catalystitservices.com>
2016-02-09 12:00:52 -0800

    Added editreportcontroller that is currently the same as the createreportcontroller with the exception of put instead of post.  Will amend and adapt tomorrow following completion of edit template

---
+++ src/main/resources/public/js/controllers/editreportcontroller.js
@@ -0,0 +1,85 @@
+angular.module('expenseApp').controller('editreportController', ['$scope', '$state', '$http', 'createLineItemService', 'currentUserService', function($scope, $state, $http, createLineItemService, currentUserService){
+	
+
+	
+	$scope.getCurrentUser = {"userId":2}; //currentUserService.getCurrentUser();
+	$scope.projectList = [];
+	$http.get('/projectsusers/' + 3).then(function(data){
+		$scope.data = data;
+		$scope.curProject = {};
+		angular.forEach($scope.data.data, function(value, key){
+			$scope.curProject = value;
+			angular.forEach(value.endUsers, function(value, key){
+				if(value.userId == $scope.getCurrentUser.userId){
+					//console.log(value.userId);
+					//console.log($scope.getCurrentUser.userId);
+					$scope.projectList.push($scope.curProject);
+				}
+			});
+		});
+		console.log($scope.projectList);
+	});
+	
+
+	
+	$scope.itemList = [];
+	$scope.itemType = 1;
+	$scope.cost = '';
+		
+	$scope.addRow = function(){
+		$scope.itemList.push({'type':{'typeId':$scope.itemType}, 'value':$scope.cost});
+		$scope.itemType = 1;
+		$scope.cost = '';
+	};
+	$scope.typeDisplay = function(typeID){
+		if(typeID == 1){
+			return "Mileage";
+		}
+		else if(typeID == 2){
+			return "Per Diem";
+		}
+		else if(typeID == 3){
+			return "Lodging"
+		}
+		else if(typeID == 4){
+			return "Travel"
+		}
+		else if(typeID == 5){
+			return "Meals"
+		}
+		else if(typeID == 6){
+			return "Entertainment"
+		}
+		else if(typeID == 7){
+			return "Parking"
+		}
+		else if(typeID == 8){
+			return "Other"
+		}
+	};
+	$scope.removeItem = function(item){
+		var index = $scope.itemList.indexOf(item);
+		$scope.itemList.splice(index, 1);
+	};
+	$scope.sendReport = function() {
+        var lineItemsList = $scope.itemList;
+        $scope.error = false;
+		var userData = angular.toJson({
+				endUser:{"userId":$scope.getCurrentUser.userId},
+        		name:reportName.value,
+        		note:note.value,
+        		project:{"projectId":projectName.value},
+        		reportStatus:{"reportStatusId":1},
+        		lineItems:lineItemsList
+            })
+            $http.put("/reports", userData).
+            success(function(data, status, headers, config){
+            	console.log("succss");
+            	window.location="#/home";
+            }).
+            error(function(data, status, headers, config){
+            	console.log("fail");
+            	$scope.error = true;
+            });
+    };
+}]);
