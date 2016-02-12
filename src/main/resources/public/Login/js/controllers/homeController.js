72d963eb9bff04fc0e3c1317c9eac8c3750dc801
Luke <lDahlberg@catalystitservices.com>
2016-02-12 08:24:02 -0800

    Added new files for format

---
+++ src/main/resources/public/Login/js/controllers/homeController.js
@@ -0,0 +1,26 @@
+angular.module('loginApp').controller('homeController', ['$scope', '$state', '$http', 'getReportService','currentUserService', function($scope, $state, $http, getReportService, currentUserService){
+	
+	$scope.getCurrentUser = currentUserService.getCurrentUser();
+	console.log($scope.getCurrentUser);
+	$scope.reportList = {};
+	getReportService.dbGetAll($scope.getCurrentUser.userId).then(
+			  function(success){
+				  $scope.reportList = success.data;
+				  console.log(success.data);
+				  return success.data;
+			  },function(error){
+				  console.log(error);  
+			  }
+			  );
+	
+	
+	$scope.loadView = function(id){
+		getReportService.setReport(id);
+		window.location = "#/detailView";
+	};
+	
+	$scope.loadCreate = function(){
+		window.location = "#/createReport";
+	};
+	
+}]);
---
+++ src/main/resources/public/Login/js/controllers/projectController.js
@@ -0,0 +1,43 @@
+angular.module('loginApp').controller('projectController', ['$scope', '$state', '$http', 'getUsersService', 'getDevsService', function($scope, $state, $http, getUsersService, getDevsService){
+	$http.get('/users').then(function(data){
+		$scope.data = data;
+	
+		console.log(data);
+
+	});
+	$scope.addDev = function(newDev){
+		getDevsService.addDev(newDev);
+		console.log(newDev);
+		$scope.devLists = getDevsService.getDev();
+		console.log(getDevsService.getDev());
+	};
+	$scope.projectNameAvailable = function(){
+		$http({
+			method: "GET",
+			url: '/userss/' + userName.value
+		}).then(function (data) {
+	          callback(data);
+	    });
+	}
+	$scope.sendProject = function() {
+		$scope.worked = false;
+		$scope.error = false;
+    	console.log(projectName.value);
+    	console.log(techLead.value);
+        var userData = JSON.stringify({
+        		name:projectName.value,
+                techId:{"userId":techLead.value}
+            })
+            console.log(userData)
+            $http.post("/projects", userData).
+            success(function(data, status, headers, config){
+            	console.log("success");
+            	$scope.worked = true;
+            }).
+            error(function(data, status, headers, config){
+            	console.log("fail");
+            	$scope.error = true;
+            });
+        //TO-DO will need a second post for the list of devs
+    }
+}]);
